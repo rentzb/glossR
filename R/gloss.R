ac30f456ae39e3e752447e7ec00a18468a7c77aa
Bradley Rentz <bradley.rentz@gmail.com>
2017-01-27 13:39:13 -1000

    Initial commit

---
+++ R/gloss.R
@@ -0,0 +1,18 @@
+#' gloss
+#'
+#' Prints a gloss
+#'
+#' @param data data frame that contains examples to be glossed
+#' @param option1 first option to be defined later
+#' @param option2 second option to be defined later
+#'
+#' @return a gloss
+#'
+#' @examples
+#' gloss()
+#'
+#' @export
+
+gloss <- function() {
+  print("Hello, world!")
+}
